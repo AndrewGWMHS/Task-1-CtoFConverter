fec65675c3cc5231a7f0dc39b79fc600ef3c8cee
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-30 08:45:17 -0400

    rwww'o

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,5 +1,33 @@
-public class Main {
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double temperatureC = 0;
+        double temperatureF = 0;
+        boolean valid = false;
+        double CELSIUS_CONVERSION_FRACTION = 1.8;
+
+        do {
+            System.out.println("Enter a temperature in Celsius to be converted to Fahrenheit.");
+            if (scan.hasNextDouble()) {
+               temperatureC = scan.nextDouble();
+               if (temperatureC >= -273.15) {
+                   valid = true;
+                   temperatureF = (temperatureC * CELSIUS_CONVERSION_FRACTION) + 32;
+               } else {
+                   System.out.println("Entered invalid temperature value: " + temperatureC + " , Try Again");
+                   System.out.println();
+               }
+            }
+            else {
+                scan.nextLine();
+                System.out.println("Invalid data type, Try Again");
+                System.out.println();
+            }
+        } while (!valid);
+
+        System.out.printf("%-2s %-1.2f", "Celsius:", temperatureC);
+        System.out.printf("\n%-2s %-1.2f", "Fahrenheit:", temperatureF);
     }
 }
